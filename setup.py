36f8d8dd3ad5191550d85f7828cd913b00d968e0
axelvonkamp <axelk1@gmx.de>
2024-08-19 14:14:06 +0200

    PyPI support

--- setup.py
+++ setup.py
@@ -1,12 +1,13 @@
 from setuptools import setup
 
 setup(name='optlang_enumerator',
-    version='0.0.11',
-    description='Enumeration of multiple solutions to a MILP with optlang.',
-    url='https://github.com/cnapy-org/optlang_enumerator.git',
+    version='0.0.12',
+    description='Module for enumerating minimal cut sets',
+    url='https://github.com/cnapy-org/optlang_enumerator',
     author='Axel von Kamp',
-    author_email='axelk1@gmx.de',
+    author_email='vonkamp@mpi-magdeburg.mpg.de',
     license='Apache License 2.0',
     packages=['optlang_enumerator'],
-    install_requires=['numpy==1.23', 'scipy', 'cobra>=0.26.3', 'optlang', 'efmtool_link', 'sympy>=1.12', 'swiglpk'],
+    package_dir={'optlang_enumerator': 'optlang_enumerator'},
+    install_requires=['numpy==1.23', 'scipy', 'cobra>=0.26.3', 'optlang', 'efmtool_link>=0.0.6', 'sympy>=1.12', 'swiglpk'],
     zip_safe=False)
